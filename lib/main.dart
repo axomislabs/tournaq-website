4b2d76f1fb0090b15df5dd80bc480137164193f1
Martin Adam <martin.l.adam.84@gmail.com>
2026-05-22 12:59:17 +0200

    Color scheme

--- lib/main.dart
+++ lib/main.dart
@@ -66,6 +66,7 @@ class _MyAppState extends State<MyApp> {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: colorScheme,
+        scaffoldBackgroundColor: Colors.white,
         useMaterial3: true,
         appBarTheme: AppBarTheme(
           backgroundColor: colorScheme.inversePrimary,
